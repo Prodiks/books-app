6b72e29a964e401b9abf9033358908a8266408f8
Pryaminin <508@irsural.ru>
2024-11-18 09:28:33 +0500

    Добавлен main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from src.books.router import router as books_router
+from src.pages.router import router as books_pages
+DATABASE_URL = "sqlite:///./sql_app.db"
+
+
+class App(FastAPI):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+
+app = App()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def home_page():
+    return {"message": "Hello World"}
+
+
+app.include_router(books_router)
+app.include_router(books_pages)
